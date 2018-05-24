f83f44f325ddddfc511a472f20c106c397549f7e
Yash <yashshah2398@gmail.com>
2018-05-24 18:11:54 +0800

    Final working code for 4 patches

--- Src/main.c
+++ Src/main.c
@@ -223,7 +223,7 @@ static void MX_ADC1_Init(void)
 	hadc1.Init.DiscontinuousConvMode = DISABLE;
 	hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
 	hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-	hadc1.Init.NbrOfConversion = 4;
+	hadc1.Init.NbrOfConversion = 1;
 	if (HAL_ADC_Init(&hadc1) != HAL_OK){
 		_Error_Handler(__FILE__, __LINE__);
 	}
--- Src/tactile_read.c
+++ Src/tactile_read.c
@@ -77,46 +77,48 @@ int TR_NextState(TR_HandleTypeDef *tr){
 		}else{
 			/* Reading process is still ongoing */
 			// Find row to be read [0,1,2,3]
+			uint8_t patch = tr->n_read % 4;
 			uint8_t row = (tr->n_read % 16)/4;
+
 			ADC_ChannelConfTypeDef sConfig;
-
-			/* Configure 4 channels for polling */
 			sConfig.SamplingTime = ADC_SAMPLETIME_41CYCLES_5;
-			// Select two normal ADC channels
-			for(int i = 0; i < 2; ++i){
-				sConfig.Rank = i+1;
-				sConfig.Channel = row + 4*i;
-				// Configure ADC handle with the channel to be scanned
-				if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK){
-					_Error_Handler(__FILE__, __LINE__);
-				}
-			}
-			// Enable the multiplexor
-			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_SET);
-			// Select rows
-			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, (int)(row/2) );
-			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, row%2 );
+			sConfig.Rank = 1;
 
-			// Select ADC8 and ADC9
-			for(int i = 8; i <= 9; ++i){
-				sConfig.Rank = i-5;
-				sConfig.Channel = i;
-				// Configure ADC handle with the channel to be scanned
-				if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK){
-					_Error_Handler(__FILE__, __LINE__);
-				}
+			/* Configure channel for polling */
+			switch(patch){
+			case 0:
+				sConfig.Channel = row;
+				break;
+			case 1:
+				sConfig.Channel = 4+row;
+				break;
+			case 2:
+				// Enable the multiplexor
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_SET);
+				// Select rows
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, (int)(row/2) );
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, row%2 );
+				sConfig.Channel = ADC_CHANNEL_8;
+				break;
+			case 3:
+				// Enable the multiplexor
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_15, GPIO_PIN_SET);
+				// Select rows
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, (int)(row/2) );
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, row%2 );
+				sConfig.Channel = ADC_CHANNEL_9;
+				break;
 			}
-			// Sensor is being run in discontinuous mode and so needs to
-			// be restarted every time since it stops after 1 reading.
-//			HAL_ADC_Start(&hadc1);
-			// Read 4 sensor values
-			for(int i = 0; i < 4; ++i){
-				if(HAL_ADC_PollForConversion(&hadc1, 1000) == HAL_OK){
-					tr->curr_values[tr->n_read + i] = HAL_ADC_GetValue(&hadc1);
-				}
+
+			if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK){
+				_Error_Handler(__FILE__, __LINE__);
 			}
+
+			while(HAL_ADC_PollForConversion(&hadc1, 1000) != HAL_OK);
+			tr->curr_values[tr->n_read] = HAL_ADC_GetValue(&hadc1);
+
 			// Update the number of values read
-			tr->n_read += 4;
+			tr->n_read += 1;
 			// Transit to the SETUP state when column needs to be changed
 			if(tr->n_read % 16 == 0 && tr->n_read < MAX_NUM_VALUES){
 				tr->state = TR_SETUP;
