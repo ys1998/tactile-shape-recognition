764c4c2cb42c92406819163fcbbf7d5c7981d144
Yash <yashshah2398@gmail.com>
2018-06-12 16:59:31 +0800

    Updated surface reconstruction algo and gitignore

--- 3D_models/generate.py
+++ 3D_models/generate.py
@@ -13,111 +13,141 @@ from scripts.pcd_io import load_point_cloud, save_point_cloud
 """
 Point clouds for supported 3D shapes.
 """
-def cylinder():
+def cylinder(R=3, H=10):
     # x^2 + y^2 = 9
     curved_surface = np.asarray(
-                        [(3*cos(theta), 3*sin(theta), z) 
-                            for z in np.arange(0, 10, 0.1) 
+                        [(R*cos(theta), R*sin(theta), z) 
+                            for z in np.arange(0, H, 0.1) 
                                 for theta in np.arange(0, 2*pi, 2*pi/30)])
     upper_cover = np.asarray(
                         [(r*cos(theta), r*sin(theta), 10)
-                            for r in np.arange(0, 3, 0.3)
+                            for r in np.arange(0, R, 0.3)
                                 for theta in np.arange(0, 2*pi, 2*pi/30)])
     lower_cover = np.asarray(
                         [(r*cos(theta), r*sin(theta), 0)
-                            for r in np.arange(0, 3, 0.3)
+                            for r in np.arange(0, R, 0.3)
                                 for theta in np.arange(0, 2*pi, 2*pi/30)])
     return np.concatenate([lower_cover, curved_surface, upper_cover])
 
-def sphere():
+def sphere(R=3):
     # x^2 + y^2 + z^2 = 9
-    return np.asarray([(sqrt(9-z**2)*cos(theta), sqrt(9-z**2)*sin(theta), z)
-                            for z in np.arange(-3, 3, 0.1)
+    return np.asarray([(sqrt(R**2-z**2)*cos(theta), sqrt(R**2-z**2)*sin(theta), z)
+                            for z in np.arange(-R, R, 0.1)
                                 for theta in np.arange(0, 2*pi, 2*pi/30)])
 
-def pyramid():
+def pyramid(L=4, H=6):
     # Base: 4x4 (square)
     # Height: 6
-    l1 = np.asarray([(x, -2) for x in np.arange(-2, 2, 0.1)])
-    l2 = np.asarray([(x, 2) for x in np.arange(-2, 2, 0.1)])
-    l3 = np.asarray([(-2, y) for y in np.arange(-2, 2, 0.1)])
-    l4 = np.asarray([(2, y) for y in np.arange(-2, 2, 0.1)])
+    l1 = np.asarray([(x, -L/2) for x in np.arange(-L/2, L/2, 0.1)])
+    l2 = np.asarray([(x, L/2) for x in np.arange(-L/2, L/2, 0.1)])
+    l3 = np.asarray([(-L/2, y) for y in np.arange(-L/2, L/2, 0.1)])
+    l4 = np.asarray([(L/2, y) for y in np.arange(-L/2, L/2, 0.1)])
     border = np.concatenate([l1, l2, l3, l4])
 
     base = np.asarray([(x, y, 0)
-                            for x in np.arange(-2, 2, 0.1)
-                                for y in np.arange(-2, 2, 0.1)])
+                            for x in np.arange(-L/2, L/2, 0.1)
+                                for y in np.arange(-L/2, L/2, 0.1)])
 
-    surface = np.concatenate([np.c_[border * (1-z/6), np.full(border.shape[0], z)]
-                                for z in np.arange(0.1, 6, 0.1)])
+    surface = np.concatenate([np.c_[border * (1-z/H), np.full(border.shape[0], z)]
+                                for z in np.arange(0.1, H, 0.1)])
     
     return np.concatenate([base, surface])
 
-def cube():
+def cube(L=4):
     # 4 x 4 x 4
-    l1 = np.asarray([(x, -2) for x in np.arange(-2, 2, 0.1)])
-    l2 = np.asarray([(x, 2) for x in np.arange(-2, 2, 0.1)])
-    l3 = np.asarray([(-2, y) for y in np.arange(-2, 2, 0.1)])
-    l4 = np.asarray([(2, y) for y in np.arange(-2, 2, 0.1)])
+    l1 = np.asarray([(x, -L/2) for x in np.arange(-L/2, L/2, 0.1)])
+    l2 = np.asarray([(x, L/2) for x in np.arange(-L/2, L/2, 0.1)])
+    l3 = np.asarray([(-L/2, y) for y in np.arange(-L/2, L/2, 0.1)])
+    l4 = np.asarray([(L/2, y) for y in np.arange(-L/2, L/2, 0.1)])
     border = np.concatenate([l1, l2, l3, l4])
 
     base1 = np.asarray([(x, y, 0)
-                       for x in np.arange(-2, 2, 0.1)
-                       for y in np.arange(-2, 2, 0.1)])
+                       for x in np.arange(-L/2, L/2, 0.1)
+                        for y in np.arange(-L/2, L/2, 0.1)])
 
     base2 = np.asarray([(x, y, 4)
-                       for x in np.arange(-2, 2, 0.1)
-                       for y in np.arange(-2, 2, 0.1)])
+                        for x in np.arange(-L/2, L/2, 0.1)
+                        for y in np.arange(-L/2, L/2, 0.1)])
 
     surface = np.concatenate([np.c_[border, np.full(border.shape[0], z)]
-                              for z in np.arange(0.1, 4, 0.1)])
+                              for z in np.arange(0.1, L, 0.1)])
 
     return np.concatenate([base1, surface, base2])
 
-def prism():
+def prism(L=4, H=6):
     # Base: equilateral triangle of side 4
     # Height: 6
-    l1 = np.asarray([(x, 0) for x in np.arange(0, 4, 0.1)])
-    l2 = np.asarray([(4 - r/2, r * sin(pi/3)) for r in np.arange(0, 4, 0.1)])
-    l3 = np.asarray([(r/2, r*sin(pi/3)) for r in np.arange(0, 4, 0.1)])
+    l1 = np.asarray([(x, 0) for x in np.arange(0, L, 0.1)])
+    l2 = np.asarray([(L - r/2, r * sin(pi/3)) for r in np.arange(0, L, 0.1)])
+    l3 = np.asarray([(r/2, r*sin(pi/3)) for r in np.arange(0, L, 0.1)])
     border = np.concatenate([l1, l2, l3])
 
     surface = np.concatenate([np.c_[border, np.full(border.shape[0], z)]
-                              for z in np.arange(0.1, 6, 0.1)])
+                              for z in np.arange(0.1, H, 0.1)])
     
-    offset = np.array([2, 2/sqrt(3)])
-    base = np.concatenate([offset + (border-offset)*e/4 for e in np.arange(0, 4, 0.1)]) 
+    offset = np.array([L/2, (L/2)/sqrt(3)])
+    base = np.concatenate([offset + (border-offset)*e/L for e in np.arange(0, L, 0.1)]) 
 
-    return np.concatenate([np.c_[base, np.zeros(base.shape[0])], surface, np.c_[base, np.full(base.shape[0], 6)]])
+    return np.concatenate([np.c_[base, np.zeros(base.shape[0])], surface, np.c_[base, np.full(base.shape[0], H)]])
 
-def cuboid():
+def cuboid(L=3, B=4, H=5):
     # 3 x 4 x 5
-    l1 = np.asarray([(x, 0) for x in np.arange(0, 4, 0.1)])
-    l2 = np.asarray([(x, 3) for x in np.arange(0, 4, 0.1)])
-    l3 = np.asarray([(0, y) for y in np.arange(0, 3, 0.1)])
-    l4 = np.asarray([(4, y) for y in np.arange(0, 3, 0.1)])
+    l1 = np.asarray([(x, 0) for x in np.arange(0, B, 0.1)])
+    l2 = np.asarray([(x, B) for x in np.arange(0, B, 0.1)])
+    l3 = np.asarray([(0, y) for y in np.arange(0, B, 0.1)])
+    l4 = np.asarray([(L, y) for y in np.arange(0, B, 0.1)])
     border = np.concatenate([l1, l2, l3, l4])
 
     base1 = np.asarray([(x, y, 0)
-                        for x in np.arange(0, 4, 0.1)
-                        for y in np.arange(0, 3, 0.1)])
+                        for x in np.arange(0, L, 0.1)
+                        for y in np.arange(0, B, 0.1)])
 
-    base2 = np.asarray([(x, y, 5)
-                        for x in np.arange(0, 4, 0.1)
-                        for y in np.arange(0, 3, 0.1)])
+    base2 = np.asarray([(x, y, H)
+                        for x in np.arange(0, L, 0.1)
+                        for y in np.arange(0, B, 0.1)])
 
     surface = np.concatenate([np.c_[border, np.full(border.shape[0], z)]
-                              for z in np.arange(0.1, 5, 0.1)])
+                              for z in np.arange(0.1, H, 0.1)])
 
     return np.concatenate([base1, surface, base2])
 
 """
 Generate point clouds for default models.
 """
-def generate_shape_pc(shape, save_dir):
+def generate_default(shape, save_dir):
     if shape not in globals():
         print("Shape not found.")
     else:
         # Generate point cloud
         point_cloud = globals()[shape]()
         save_point_cloud(point_cloud, os.path.join(save_dir, shape + '.pcd'))
+
+"""
+Generate point clouds of the default shapes for training.
+
+Arguments:
+    shape       : list of shapes to be generated
+    save_dir    : directory where generated models are saved
+    num_samples : number of models to be generated for each shape
+    min_pts     : minimum number of points in cloud
+    max_pts     : maximum number of points in cloud
+"""
+def generate_data(
+        shapes=['cylinder', 'sphere', 'cube', 'cuboid', 'prism', 'pyramid'],
+        save_dir='.', 
+        num_samples=1000, 
+        min_pts=200, 
+        max_pts=None
+    ):
+    assert all([x in globals() for x in shapes]), "Invalid shape found."
+    assert max_pts is None or max_pts > min_pts, "'max_pts' should be greater than 'min_pts'"
+
+    for shape in shapes:
+        pc = globals()[shape]()
+        if not os.path.exists(os.path.join(save_dir, shape)):
+            os.makedirs(os.path.join(save_dir, shape))
+        for n in range(num_samples):
+            l = min_pts; u = max_pts if max_pts is not None else pc.shape[0]
+            n_pts = int(l + np.random.choice(u-l, 1, replace=False))
+            idxs = np.random.choice(pc.shape[0], n_pts, replace=False)
+            save_point_cloud(pc[idxs], os.path.join(save_dir, shape, "%s_%d.pcd" % (shape, n)))
--- README.md
+++ README.md
@@ -3,22 +3,25 @@
 # 3D Shape Recognition using Neuromorphic Tactile Sensing
 This repository contains the code for my project at SINAPSE, National University of Singapore during May-July 2018 as a part of my research internship under the guidance of Prof. Alcimar Soares.
 
-This codebase can be broadly classified into the following sections:
+#### Contents
+* [Abstract](#abstract)
+* [Plan of action](#plan-of-action)
+* [Ideation](#ideation)
 * [Firmware](#firmware)
 * [User Interface](#user-interface)
 * [Deep Learning algorithm](#deep-learning-algorithm)
 
-### Abstract
+## Abstract
 The main aim of my project was to design a deep learning architecture for *three-dimensional shape recognition* based on neuromorphic data collected from tactile sensor arrays mounted on a prosthetic hand, and to create a framework for real-time shape recognition.
 
 We used neuromorphic tactile sensors based on the *piezoresistive principle* to encode changes in pressure as spike events at each sensing element, and used a set of such spike streams to deduce the shape of the object being palpated. The responses of neuromorphic sensors resemble spikes from neurons in the human skin, and provide an efficient way to encode only the necessary information for further processing. 
 
 During this internship, we developed a deep learning model to extract information from the stream of spikes and generalize it according to the object being grasped. We also endeavored to design an algorithm to dynamically determine the manner in which the object needs to be palpated in order to maximize information gain for shape recognition (i.e *active palpation*).
 
-### Plan of action
+## Plan of action
 ![plan-of-action](images/poa.png)
 
-### Ideation
+## Ideation
 Earlier approaches used *softmax classifiers* and *extreme learning machines* (ELMs) for the task of texture recognition, and were able to obtain decent accuracy for the same. I plan to use a combination of **Convolutional Neural Networks** and **Recurrent Neural Networks** for the purpose of shape recognition.
 
 CNNs and RNNs are effective in extracting spatial and temporal correlation and patterns respectively. We can use the spike data to generate a 2D heat-map representing the pressure measured by each sensing element at a given instant (ie. for a given palpating position), and then extend this information across time by generating similar maps for other palpating positions. Since this information has both spatial and temporal aspects, a combination of CNN and RNN (similar to that used for video classification) could be a good approach for tackling the problem. We could use independent models for each tactile sensor (ie. a separate model for each finger) and then combine their predictions using a classifier to get the final output (recognized shape).
@@ -35,4 +38,23 @@ This section contains the firmware (resides [here](MainController_new)) for read
 
 ## User Interface
 
-## Deep Learning Algorithm
+## Algorithm
+
+### Data preprocessing
+In order to generate the training data (`.pcd` files for point clouds)
+```
+$ cd 3D_models
+$ python -i generate.py 
+>>> generate_data()
+```
+For extracting point-of-view images from the generated data
+```
+$ cd scripts
+$ python extract_pov.py ../3D_models ../save
+```
+For converting the PoV images to `numpy` arrays for training
+```
+$ cd shape_recognition
+$ python preprocess.py ../save
+```
+
