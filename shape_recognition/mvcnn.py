793d403539fca3f59bd1768b3c68c422902561ed
Yash <yashshah2398@gmail.com>
2018-06-12 11:07:01 +0800

    Updated deep learning model

---
+++ shape_recognition/mvcnn.py
@@ -0,0 +1,21 @@
+"""
+Multi-View Convolutional Neural Network (MVCNN) implementation in Tensorflow.
+"""
+
+import tensorflow as tf
+import numpy as np
+
+class MVCNN(object):
+    def __init__(self):
+
+
+
+    def train(self, X, Y):
+
+
+
+
+    def predict(self, X):
+
+
+
