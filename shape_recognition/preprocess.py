d4470dafe5ab9867cfba8b467fb65c982860ad2d
Yash <yashshah2398@gmail.com>
2018-06-12 13:33:23 +0800

    Added preprocessing scripts

--- shape_recognition/preprocess.py
+++ shape_recognition/preprocess.py
@@ -1,63 +1,98 @@
 """
-Functions for preprocessing spike data from files and preparing
-input batches for training.
-Also has functions for loading preprocessed data.
+Script for preparing input data for training.
+
+It scans the specified directory for PoV images and converts
+them into 128x128 numpy arrays by cropping the central square,
+rescaling and converting to grayscale.
+
+Output:
+(i) data
+    a numpy array of dimension (N, 6, 128, 128) 
+
+(ii) labels
+    a numpy array of dimension (N, M) with labels as below
+        0 - cube
+        1 - cuboid
+        2 - cylinder
+        3 - prism
+        4 - pyramid
+        5 - sphere
+
+    where   N = number of 3D models
+            M = number of shapes/classes
 """
 
 import numpy as np
-import sys, os
-import argparse
+from scipy import misc
+from skimage.transform import resize
+import matplotlib.pyplot as plt
+import os, sys
 
-# Commandline arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('--file', type=str, default='', help='Path to data-file to be preprocessed')
-parser.add_argument('--folder', type=str, default='', help='Path to folder containing data-files')
-parser.add_argument('--save_dir', type=str, default='data', help='Path to directory where preprocessed data is saved')
+LABELS = {
+    'cube':0,
+    'cuboid':1,
+    'cylinder':2,
+    'prism':3,
+    'pyramid':4,
+    'sphere':5
+}
 
-""" Function for processing a single file """
-def process_file(path, shape, save_dir):
-    # Create directory if it doesn't exist
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-
-    # Create data placeholder
-    data = np.full(shape, 0, dtype=np.float)
-    with open(path, 'r') as f:
-        # Skip the header
-        f.readline()
-        # Iterate over lines in the file
-        for line in f:
-            patchNum, row, col, pol, t = [int(x) for x in line.split()]
-            data[0][t][row][col + 4*patchNum] = pol
-
-
+def make_label(idx):
+    val = np.zeros(len(LABELS))
+    val[idx] = 1.0
+    return val
 
 def main():
-    args = parser.parse_args()
-    assert args.file is not '' and args.folder is not '', "Enter valid file/folder name."
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print("Usage:\npython preprocess.py [DATA_DIR] [SAVE_DIR]")
+        exit(1)
+    data_dir = sys.argv[1]
+    if not os.path.exists(data_dir):
+        print("Invalid data directory. Aborting.")
+        exit(1)
+    if len(sys.argv) == 2:
+        save_dir = 'data'
+    else:
+        save_dir = sys.argv[2]
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    
+    # Placeholders for output
+    data = []
+    labels = []
 
-    if args.folder:
-        # Process all valid .txt files inside specified folder
-        for root, _, files in os.walk(args.folder):
-            for fl in files:
-                path = os.path.join(root, fl)
-                if os.path.splitext(path)[1] == '.txt':
-                    with open(path, 'r') as f:
-                        flag, t = f.readline().split()
-                        if flag == 'TACTILE_DATA_FILE':
-                            # Input tensor shape: [batch_size, depth, height, width]
-                            shape = [1, int(t), 4, 16]
-                            # File is valid, read data 
-                            process_file(path, shape, args.save_dir)
-    elif args.file:
-        with open(args.file, 'r') as f:
-            flag, t = f.readline().split()
-            if flag == 'TACTILE_DATA_FILE':
-                # Input tensor shape: [batch_size, depth, height, width]
-                shape = [1, int(t), 4, 16]
-                # File is valid, read data
-                process_file(args.file, shape, args.save_dir)
+    # Walk through the data directory
+    for root, _, files in os.walk(data_dir):
+        chunk = None; cntr=0
+        for f in files:
+            name, ext = os.path.splitext(f)
+            # check if the file is valid
+            if name.startswith('view') and ext == '.png':
+                cntr += 1
+                if chunk is None:
+                    chunk = np.empty([6, 128, 128])
+                # Load file
+                img = misc.imread(os.path.join(root, f),flatten=True)
+                # Crop central square
+                side = min(img.shape)
+                x = img.shape[0]//2 - side//2
+                y = img.shape[1]//2 - side//2
+                img = img[x:x+side, y:y+side]
+                # Rescale image to 128x128
+                img = resize(img, (128, 128), anti_aliasing=True, preserve_range=True)
+                chunk[int(name.split('_')[1])] = img
+        if chunk is not None and cntr == 6:
+            data.append(chunk)
+            labels.append(make_label(LABELS[os.path.split(root)[1].split('_')[0]]))
+    
+    # Convert output into a single array
+    data = np.stack(data)
+    labels = np.stack(labels)
 
+    # Save to disk
+    np.save(os.path.join(save_dir, 'data.npy'), data)
+    np.save(os.path.join(save_dir, 'labels.npy'), labels)
+    print("Preprocessing done.")
 
 if __name__ == '__main__':
     main()
